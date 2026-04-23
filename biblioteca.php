3b61d53bc54bc37c4b241996cdddc52a15265edb
Maria Breda <maria.e.oliveira33@edu.senai.br>
2026-04-23 12:40:15 -0300

    V.0.1 - Acrescentado linha do aprendizado

--- biblioteca.php
+++ biblioteca.php
@@ -2,15 +2,3 @@
 
 //funções em PHP
 echo "na aula do dia 23/04/2026, eu aprendi a mexer no github/git e desenvolver funções no php =D \n";
-
-function somar($a, $b, $c)
-{
-    return $a + $b;
-}
-
-function subtrair($a, $b)
-{
-    return $a - $b;
-}
-
-echo "resultado: ", subtrair(10, 5);
