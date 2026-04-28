057a5cc7b03f240e6d35d779e11a1e3904430205
Maria Breda <maria.e.oliveira33@edu.senai.br>
2026-04-28 15:44:06 -0300

    Versão 4

--- bibliotecaFuncoes.php
+++ bibliotecaFuncoes.php
@@ -1,6 +1,62 @@
 <?php
 
-namespace sistemageometria {
+
+namespace geometria;
+
+function areaQuadrado($lado)
+{
+    return $lado * $lado;
+}
+
+function  areaRetangulo($base, $altura)
+{
+    return $base * $altura;
+}
+
+function  areaTriangulo($base, $altura)
+{
+    return $base * $altura / 2;
+}
+
+function   areaCirculo($raio)
+{
+    return $raio * $raio;
+}
+
+function  areaTrapezio($baseMaior, $baseMenor, $altura)
+{
+    return ($baseMaior + $baseMenor) * $altura / 2;
+}
+
+
+namespace conversao;
+
+function dolarParaReal($dolar, $real)
+{
+    return $dolar / $real;
+}
+
+function euroParaReal($euro, $real)
+{
+    return $euro / $real;
+}
+
+function  pesoParaReal($peso, $real)
+{
+    return $peso / $real;
+}
+
+function libraParaReal($libra, $real)
+{
+    return $libra / $real;
+}
+
+function ieneParaReal($iene, $real)
+{
+    return $iene / $real;
+}
+
+namespace sistemageometria; {
 
     function calcularImc($peso, $altura)
     {
--- entradaDados.php
+++ entradaDados.php
@@ -2,14 +2,82 @@
 
 require_once "bibliotecaFuncoes.php";
 
+use function conversao\dolarParaReal;
+use function conversao\euroParaReal;
+use function conversao\pesoParaReal;
+use function conversao\libraParaReal;
+use function conversao\ieneParaReal;
 use function sistemageometria\calcularImc;
 use function sistemageometria\valorIdealAgua;
 use function sistemageometria\frequenciaCardiacaMaxima;
 use function sistemageometria\converterLibrasParaQuilo;
 use function sistemageometria\calcularCaloriasBasais;
 
-echo "Valor do imc: ", calcularImc(45, 1.60), "\n";
-echo "Valor ideal de agua: ", valorIdealAgua(45), "\n";
-echo "Valor frequencia cardiaca maxima: ", frequenciaCardiacaMaxima(16), "\n";
-echo "Valor em quilos: ", converterLibrasParaQuilo(130), "\n";
-echo "Valor calorias basais: ", calcularCaloriasBasais(45, 16, 1.60, "Masculino"), "\n";
+
+
+
+$op = "";
+$opcoes = "";
+
+while ($opcoes != "4") {
+    echo "\n Escolha uma das categorias: \n 1.Conversão \n 2.Geometria \n 3.Saúde \n 4.Sair";
+
+    $opcoes = readline(">>");
+
+    switch ($opcoes) {
+
+        case 1:
+            while ($op != "6") {
+                echo "\n Menu: \n 1.Conversão para dólar \n 2.Conversão para euro \n 3.Conversão para peso \n 4.Conversão para libra \n 5.Conversão para iene \n 6.Sair \n";
+
+                $op = readline(">>");
+                switch ($op) {
+                    case 1:
+                        $dolar = readline("Digite a quantidade de dólares:");
+                        $dolarcotacao = readline("Digite o valor atual do dólar");
+
+                        echo "Dólar para real:", dolarParaReal($dolar, $dolarcotacao), "\n\n";
+                        break;
+
+                    case 2:
+                        $euro = readline("Digite a quantidade de euros:");
+                        $eurocotacao = readline("Digite o valor atual do euro");
+
+                        echo "euro para real:", euroParaReal($euro, $eurocotacao), "\n\n";
+                        break;
+
+
+                    case 3:
+                        $peso = readline("Digite a quantidade de euros:");
+                        $pesocotacao = readline("Digite o valor atual do euro");
+
+                        echo "Peso para real:", pesoParaReal($peso, $pesocotacao), "\n\n";
+                        break;
+
+
+                    case 4:
+                        $libra = readline("Digite a quantidade de libras:");
+                        $libracotacao = readline("Digite o valor atual da libra");
+
+                        echo "Libra para real:", libraParaReal($libra, $libracotacao), "\n\n";
+                        break;
+
+
+                    case 5:
+                        $iene = readline("Digite a quantidade de libras:");
+                        $ienecotacao = readline("Digite o valor atual da libra");
+
+                        echo "Iene para real:", ieneParaReal($iene, $ienecotacao), "\n\n";
+                        break;
+
+                    case 6:
+                        echo "Saindo...";
+                        break;
+
+                    default:
+                        echo "Opção invalida!";
+                        break;
+                }
+            }
+    }
+}
