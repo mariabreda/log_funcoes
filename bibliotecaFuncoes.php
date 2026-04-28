52776224806528cfd198940ec8cfa5d8ca3545db
Maria Breda <maria.e.oliveira33@edu.senai.br>
2026-04-28 09:37:22 -0300

    Versão Final

---
+++ bibliotecaFuncoes.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace conversao {
+
+    function dolarParaReal($valor, $cotacao)
+    {
+        return $valor * $cotacao;
+    }
+
+    function europarareal($valor, $cotacao)
+    {
+        return $valor * $cotacao;
+    }
+
+    function pesoParaReal($valor, $cotacao)
+    {
+        return $valor / $cotacao;
+    }
+
+    function  libraParaReal($valor, $cotacao)
+    {
+        return $valor / $cotacao;
+    }
+
+    function  ieneParaReal($valor, $cotacao)
+    {
+        return $valor / $cotacao;
+    }
+}
--- entradaDados.php
+++ entradaDados.php
@@ -8,8 +8,9 @@ use function conversao\ieneParaReal;
 use function conversao\libraParaReal;
 use function conversao\pesoParaReal;
 
+//Atividade 1
 echo "Valor do dólar: ", dolarParaReal(100, 5), "\n";
 echo "Valor do euro: ", europarareal(100, 5.85), "\n";
-echo "Valor do peso: ", pesoParaReal(100, 0, 0035), "\n";
-echo "Valor da libra: ", libraParaReal(100, 6, 74), "\n";
-echo "Valor do iene: ", ieneParaReal(100, 0, 031), "\n";
+echo "Valor do peso: ", pesoParaReal(100, 0.0035), "\n";
+echo "Valor da libra: ", libraParaReal(100, 6.74), "\n";
+echo "Valor do iene: ", ieneParaReal(100, 0.031), "\n";
